ac11c9e9de5cfc8510f604bc8dc3faf521836e8f
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-17 22:31:05 +0200

    Concat
    Script qui renvoie, à partir d'un tableau d'arguments, une chaîne de caractères dont le séparateur est le dernier argument de ce tableau.

---
+++ air.02.js
@@ -0,0 +1,49 @@
+/*Fonctions*/
+const concatString = (array, lastArgument) => {
+    const stringArray = array
+    let result = ""
+
+    for (i = 0; i < stringArray.length - 1; i++) {
+        if (i === stringArray.length - 2) {
+            result = result + stringArray[i]
+            break
+        }
+
+        result = result + stringArray[i] + lastArgument
+    }
+
+    return result
+}
+
+/*Gestion des erreurs*/
+const isValidArguments = (arguments) => {
+    if (arguments.length >= 3) {
+        return arguments
+    } else {
+        return console.log("Une erreur est survenue. Veuillez renseigner au moins trois arguments.")
+    }
+}
+
+const isValidString = (string) => {
+    if (isNaN(string)) {
+        return string.toString()
+    } else {
+        return console.log("Une erreur est survenue.")
+    }
+}
+
+/*Parsing*/
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+/*Résolution*/
+const displayFullString = () => {
+    const strings = isValidArguments(getArguments())
+
+    return console.log(concatString(strings, strings[strings.length - 1]))
+}
+
+/*Affichage du résultat*/
+displayFullString()
