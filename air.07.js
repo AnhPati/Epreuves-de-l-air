acf817df40b9548bb640fd7a0e250e7c63399ab4
Anh Pati <romain-felicianne@hotmail.fr>
2023-07-18 15:26:54 +0200

    Insertion dans un tableau trié
    Script qui insert le nombre passé dans le dernier argument du tableau, à sa place dans un tableau d'arguments trié.

---
+++ air.07.js
@@ -0,0 +1,61 @@
+/*Fonctions*/
+const insertSort = (numbers, number) => {
+    let result = numbers
+    result.splice(result.length - 1, 1)
+
+    for (i = 0; i < numbers.length - 1; i++) {
+
+        if (numbers[i] > number) {
+            result.splice(i, 0, number)
+            break
+        }
+    }
+
+    return result.join(' ')
+}
+
+/*Gestion des erreurs*/
+const isValidArguments = (arguments) => {
+    if (arguments.length >= 3) {
+        return arguments
+    } else {
+        return console.log("Une erreur est survenue. Veuillez renseigner au moins trois arguments.")
+    }
+}
+
+const isValidNumber = (number) => {
+    if (!isNaN(parseInt(number, 10))) {
+        return Number(number)
+    } else {
+        return console.log("Une erreur est survenue.")
+    }
+}
+
+/*Parsing*/
+const getArguments = () => {
+    const arguments = process.argv.slice(2)
+    return arguments
+}
+
+/*Résolution*/
+const displayInsertSort = () => {
+    { }
+    let numbers = isValidArguments(getArguments())
+
+    if (!numbers) {
+        return
+    }
+
+    for (i = 0; i < numbers.length; i++) {
+        if (!isValidNumber(numbers[i])) {
+            return
+        } else {
+            numbers[i] = isValidNumber(numbers[i])
+        }
+    }
+
+    return console.log(insertSort(numbers, numbers[numbers.length - 1]))
+}
+
+/*Affichage du résultat*/
+displayInsertSort()
